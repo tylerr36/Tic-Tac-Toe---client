005d023c4cc3a0036c249ba0dc66a480792ca7b7
TylerR36 <tylerrotman@gmail.com>
2019-09-26 14:56:54 -0400

    Added auth info for index.html, app.js, events.js
    Added exports to events.js, authEvents to app.js for all buttons,
    added success failure functions/messages to ui.js

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -2,14 +2,19 @@
 
 // use require with a reference to bundle the file and use it in this file
 // const example = require('./example')
+const authEvents = require('./auth/events')
 
 // use require without a reference to ensure a file is bundled
 // require('./example')
 
 $(() => {
-  $('#sign-up').on('submit', function (event) {
-    event.preventDefault()
-    console.log(event.target)
-  })
+  $('#sign-up').on('submit', authEvents.onSignUp)
+  $('#sign-in').on('submit', authEvents.onSignIn)
+  $('#change-password').on('submit', authEvents.onChangePassword)
+  $('#sign-out').on('submit', authEvents.onSignOut)
+  // $('#sign-up').on('submit', function (event) {
+  // event.preventDefault()
+  // console.log(event.target)
+  // })
   // your JS code goes here
 })
