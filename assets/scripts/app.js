32150219aad6528b1d056eb50711a6f189a85bc5
TylerR36 <tylerrotman@gmail.com>
2019-09-30 15:09:57 -0400

    Sign in/up confirmations now appear. Can now only click each square once. Added vmin to game board in scss.

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -3,16 +3,18 @@
 // use require with a reference to bundle the file and use it in this file
 // const example = require('./example')
 const authEvents = require('./game/events')
+const gameEvents = require('./game/gamelogic')
 // const gameEvents = require('./game/gamelogic')
 // use require without a reference to ensure a file is bundled
 // require('./example')
 
 $(() => {
   $('#sign-up').on('submit', authEvents.onSignUp)
+  console.log(authEvents.onSignUp)
   $('#sign-in').on('submit', authEvents.onSignIn)
   $('#change-password').on('submit', authEvents.onChangePassword)
   $('#sign-out').on('submit', authEvents.onSignOut)
-  $('.border').on('click', authEvents.onClick)
+  $('.border').on('click', gameEvents.onClick)
   // $('#board').on('click', gameEvents.playGame)
 
   // $('#sign-up').on('submit', function (event) {
